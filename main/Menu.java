12ac8885db6ce5b963dd765e5ee95d98cf038b0c
Kaposvári Gergő János <kaposvari.gergo.2003.23@gmail.com>
2023-11-22 21:39:28 +0100

    Made new game menu

--- main/Menu.java
+++ main/Menu.java
@@ -2,11 +2,32 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+import java.util.Objects;
 
 
 public class Menu extends JFrame{
 
     private static CardLayout cardLayout;
+    private final JComboBox<Object> gridSelector = new JComboBox<>(availableGrids);
+    private final JComboBox<Object> ruleSelector = new JComboBox<>();
+
+    private static final Object[] availableGrids = new Object[3];
+    private static final Object[] rectangleRule = new Object[1];
+    private static final Object[] hexagonRule = new Object[1];
+    private static final Object[] triangleRule = new Object[1];
+
+    static{
+        availableGrids[0] = "Négyszög alapú pálya";
+        availableGrids[1] = "Hatszög alapú pálya";
+        availableGrids[2] = "Háromszög alapú pálya";
+
+        rectangleRule[0] = "B3/S23";
+        hexagonRule[0] = "B3/S23";
+        triangleRule[0] = "B3/S23";
+    }
+
+
 
     public Menu(){
         super("Game of life");
@@ -15,19 +36,18 @@ public class Menu extends JFrame{
 
     public void startMenu(){
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setSize(400, 200);
+        this.setSize(600, 250);
+        this.setResizable(false);
         JPanel cardPanel = new JPanel();
         cardPanel.setLayout(cardLayout);
 
-        JPanel newGamePanel = new JPanel();
-        newGamePanel.add(new JLabel("Új játék"));
+        JPanel newGamePanel = new JPanel(new BorderLayout());
         cardPanel.add(newGamePanel, "newGame");
 
         JButton newGameButton = new JButton("Új játék");
         newGameButton.addActionListener(e -> cardLayout.show(cardPanel, "newGame"));
 
         JPanel loadGamePanel = new JPanel();
-        loadGamePanel.add(new JLabel("Játék betöltése"));
         cardPanel.add(loadGamePanel, "loadGame");
 
         JButton loadGameButton = new JButton("Játék betöltése");
@@ -42,11 +62,41 @@ public class Menu extends JFrame{
         JPanel exitPanel = new JPanel();
         exitPanel.add(exitButton);
 
+        gridSelector.addActionListener(e -> updateRuleSelector());
+
+        JPanel comboBoxPanel = new JPanel();
+        comboBoxPanel.add(gridSelector);
+        comboBoxPanel.add(ruleSelector);
+
+        newGamePanel.add(comboBoxPanel, BorderLayout.CENTER);
+
+
+        JButton startButton = new JButton("Játék indítása a megadott beállításokkal");
+        startButton.addActionListener(e -> startNewGame());
+        newGamePanel.add(startButton, BorderLayout.SOUTH);
+
 
         this.add(buttonPanel, BorderLayout.NORTH);
         this.add(cardPanel, BorderLayout.CENTER);
         this.add(exitPanel, BorderLayout.SOUTH);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+    }
+
+    public void startNewGame(){
+        System.out.println("New game \nPálya: " + gridSelector.getSelectedItem() + "\nSzabály: " + ruleSelector.getSelectedItem());
+    }
+
+    public void updateRuleSelector(){
+            Object grid = gridSelector.getSelectedItem();
+            DefaultComboBoxModel<Object> model = new DefaultComboBoxModel<>();
+            if(Objects.equals(grid, availableGrids[0])){
+                model.addAll(List.of(rectangleRule));
+            }else if(Objects.equals(grid, availableGrids[1])){
+                model.addAll(List.of(hexagonRule));
+            }else{
+                model.addAll(List.of(triangleRule));
+            }
+            ruleSelector.setModel(model);
     }
 }
